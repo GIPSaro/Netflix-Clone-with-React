0f161f765e291f3888cc21cfc7e4505a7afb67c5
Giorgia Ipsaro Passione <145654021+GIPSaro@users.noreply.github.com>
2024-06-28 16:50:54 +0200

    commit ProfilePage

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import "./App.css";
 import HeroSection from "./components/HeroSection";
 import MyNav from "./components/MyNav";
 import MyFooter from "./components/MyFooter";
-
+/* import ProfilePage from "./components/ProfilePage"; */
 function App() {
   return (
     <div className="App">
       <MyNav />
-
+      {/* <ProfilePage/> */}
       <HeroSection />
 
       <MyFooter />
